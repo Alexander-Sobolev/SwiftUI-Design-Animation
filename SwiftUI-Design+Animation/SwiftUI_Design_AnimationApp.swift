5f99983243cfcc31898346896fee1a3ece777c65
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-10-01 13:31:29 +0300

    Initial Commit

---
+++ SwiftUI-Design+Animation/SwiftUI_Design_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Design_AnimationApp.swift
+//  SwiftUI-Design+Animation
+//
+//  Created by Alexander Sobolev on 01.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Design_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
