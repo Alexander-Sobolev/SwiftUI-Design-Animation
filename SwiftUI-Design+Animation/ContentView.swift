75d4b56ff61e43f87188870dacb28b664610fab2
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-10-03 19:35:50 +0300

    Add second card

--- SwiftUI-Design+Animation/ContentView.swift
+++ SwiftUI-Design+Animation/ContentView.swift
@@ -9,37 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                VStack {
-                    Text("Design SwiftUI")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                    
-                    Text("Sertificate")
-                        .foregroundColor(Color("Primary"))
-                }
-                
+        ZStack {
+            
+            VStack {
                 Spacer()
+            }
+            .frame(width: 300, height: 220)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .offset(x: 0, y: -20)
+            
+            VStack {
+                HStack {
+                    VStack {
+                        Text("Design SwiftUI")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        
+                        Text("Sertificate")
+                            .foregroundColor(Color("Primary"))
+                    }
+                    
+                    Spacer()
+                    
+                    Image("Logo SwiftUI")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
                 
-                Image("Logo SwiftUI")
+                Image("2")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 50)
-            } 
-            .padding(.horizontal, 20)
-            .padding(.top, 20)
-            
-            Image("2")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 300, height: 110, alignment: .top)
+                    .frame(width: 300, height: 110, alignment: .top)
+            }
+            .frame(width: 340, height: 220)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
         }
-        .frame(width: 340, height: 220)
-        .background(Color.black)
-        .cornerRadius(20)
-        .shadow(radius: 20)
     }
 }
 
