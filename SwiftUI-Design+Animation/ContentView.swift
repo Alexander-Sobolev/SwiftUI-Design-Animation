e154de14089c930001666d1fbc766fa68dc47560
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-10-04 21:06:38 +0300

    Add blendMode to MainCard

--- SwiftUI-Design+Animation/ContentView.swift
+++ SwiftUI-Design+Animation/ContentView.swift
@@ -38,6 +38,7 @@ struct ContentView: View {
                 .blendMode(.hardLight)
             
             CardView()
+                .blendMode(.hardLight)
             
             BottomCardView()
                 .blur(radius: 20)
