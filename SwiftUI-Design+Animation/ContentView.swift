70c8d12d951113c6e97a9f95f2f91d3cbd2a6bdd
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-10-15 20:31:00 +0300

    Add @State property wrapper

--- SwiftUI-Design+Animation/ContentView.swift
+++ SwiftUI-Design+Animation/ContentView.swift
@@ -8,40 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var show = false
+    
     var body: some View {
         ZStack {
             TitleView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20: 0)
+                .animation(.default)
             
             BackCardView()
-                .background(Color(#colorLiteral(red: 1, green: 0.2007738416, blue: 0.409167171, alpha: 1)))
+                .background(show ? Color(#colorLiteral(red: 1, green: 0.2007738416, blue: 0.409167171, alpha: 1)): Color(#colorLiteral(red: 0.3026444238, green: 0.09493814553, blue: 1, alpha: 1)))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -40)
+                .offset(x: 0, y: show ? -400: -40)
                 .scaleEffect(0.9)
-                .rotationEffect(.degrees(10))
+                .rotationEffect(.degrees(show ? 0: 10))
                 .rotation3DEffect(
                     .degrees(10),
                     axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
             
             BackCardView()
-                .background(Color(#colorLiteral(red: 0.3762339139, green: 0.1589064367, blue: 1, alpha: 1)))
+                .background(show ? Color(#colorLiteral(red: 0.3026444238, green: 0.09493814553, blue: 1, alpha: 1)): Color(#colorLiteral(red: 1, green: 0.2007738416, blue: 0.409167171, alpha: 1)))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -20)
+                .offset(x: 0, y: show ? -200: -20)
                 .scaleEffect(0.95)
-                .rotationEffect(.degrees(5))
+                .rotationEffect(.degrees(show ? 0: 5))
                 .rotation3DEffect(
                     .degrees(5),
                     axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
             
             CardView()
                 .blendMode(.hardLight)
+                .onTapGesture {
+                    self.show.toggle()
+                }
             
             BottomCardView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20: 0)
+                .animation(.default) // 3620
         }
     }
 }
